b4f850f94201b897a981045a7f426dfa4f73db54
Lucie Volemanová <lucievoleman@gmail.com>
2024-09-30 19:57:44 +0200

    Příjem divadla

---
+++ script.js
@@ -0,0 +1,10 @@
+var listek = 12
+var navstevnici = 174
+var pocetPredstaveni = 15
+var prijem = listek*navstevnici*pocetPredstaveni
+document.body.innerHTML += "<p>" + prijem + " €" +"</p>"
+var studenti = 12*0.65
+var studenti40procet = studenti*navstevnici*pocetPredstaveni*0.4
+var plneVstupne60procent = prijem*0.6
+var prijemSeStudenty = plneVstupne60procent+studenti40procet
+document.body.innerHTML += "<p>" + Math.floor(prijemSeStudenty) + " €" +"</p>"
